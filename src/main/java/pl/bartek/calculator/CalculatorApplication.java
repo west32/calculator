118e224800086c03381677e458809c45e2921519
Bartłomiej Juda <bartekjkk@gmail.com>
2023-03-24 14:07:43 +0100

    Initial commit

---
+++ src/main/java/pl/bartek/calculator/CalculatorApplication.java
@@ -0,0 +1,18 @@
+package pl.bartek.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorApplication.class, args);
+	}
+
+}
+
+//TODO
+//podpiac ten projekt do nowego repo
+//poprawiac endpoint (helloworld/ czasownik) zeby z  konwencją z ksiazki w osobnym pullrequescie :D
+//DLa Michcia i Pytka wytlumaczyc Bartkowi dlaczego zmiana portu na 9090 itp
---
+++ src/main/java/pl/bartek/calculator/controller/CalculatorController.java
@@ -0,0 +1,5 @@
+package pl.bartek.calculator.controller;
+
+public class CalculatorController {
+
+}
