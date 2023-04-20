eaefe417a7ca7b988701e60d894559befee71836
Bartłomiej Juda <bartekjkk@gmail.com>
2023-04-20 14:07:09 +0200

    advent callendar API

---
+++ src/main/java/pl/bartek/calculator/controller/AdventCallendarController.java
@@ -0,0 +1,30 @@
+package pl.bartek.calculator.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import pl.bartek.calculator.service.ServiceAdventCallendar;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Controller
+public class AdventCallendarController {
+
+    @PostMapping("/advent-callendar")
+    @ResponseBody
+
+    public int result(@RequestBody List<Integer> data ){
+
+        ServiceAdventCallendar serviceAdventCallendar = new ServiceAdventCallendar();
+        return serviceAdventCallendar.countIncreasment(data);
+    }
+
+//    TODO zamiast hardode zawartosc listy to musze je przeslac w postaci json
+//    Lista integerow w jsonie co to jak ja zrobic, jak bede przzesylac liste intow w jsonie to spring
+//     zmapuje na taka kolekcje jaka wpisalem
+
+
+}
---
+++ src/main/java/pl/bartek/calculator/service/ServiceAdventCallendar.java
@@ -0,0 +1,31 @@
+package pl.bartek.calculator.service;
+
+import java.util.Collection;
+import java.util.List;
+
+public class ServiceAdventCallendar {
+
+        public int countIncreasment(List<Integer> data){
+            int counter = -1;
+            Integer previousElement = 0;
+            Integer actualElement;
+
+            for (int i = 0; i < data.size(); i++ ){
+
+                actualElement = data.get(i);
+                if (actualElement > previousElement){
+                    counter++;
+                }
+                previousElement = actualElement;
+
+            }
+            return counter;
+        }
+
+
+    }
+
+
+
+
+
