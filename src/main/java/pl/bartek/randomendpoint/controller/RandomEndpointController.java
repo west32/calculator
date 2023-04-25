c459d788c803de564936ffd9aa946e19261d905a
Bartłomiej Juda <bartekjkk@gmail.com>
2023-04-25 15:31:31 +0200

    random endpoint

--- src/main/java/pl/bartek/randomendpoint/controller/RandomEndpointController.java
+++ src/main/java/pl/bartek/randomendpoint/controller/RandomEndpointController.java
@@ -1,25 +1,18 @@
 package pl.bartek.randomendpoint.controller;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import pl.bartek.randomendpoint.service.RandomEndpointService;
-
-@Controller
+@RestController
 public class RandomEndpointController {
-
-
-
     @GetMapping("/random-endpoint")
     @ResponseBody
 //    public RandomEndpointService randomEndpoint(@RequestParam(name = "name", required = false, defaultValue = "anymous")String name){
-//
 //        return new RandomEndpointService("Bartek", 1);
 //    }
-
-
-    public String hello (){
+    public String hello(){
         return "Hello";
     }
 }
--- src/main/java/pl/bartek/randomendpoint/service/RandomEndpointService.java
+++ src/main/java/pl/bartek/randomendpoint/service/RandomEndpointService.java
@@ -4,13 +4,10 @@ public class RandomEndpointService {
     private final String Hello;
     private final int id;
 
-
-
     public RandomEndpointService(String Hello, int id) {
         this.Hello = Hello;
         this.id = id;
     }
-
     public String getHello() {
         return Hello;
     }
