34b79bc258747be85eaad85d004bf418c130c281
Bartłomiej Juda <bartekjkk@gmail.com>
2023-05-06 11:45:50 +0200

    refactor

--- src/main/java/pl/bartek/searchcar/controller/SearchCarResponse.java
+++ src/main/java/pl/bartek/searchcar/controller/SearchCarResponse.java
@@ -6,11 +6,9 @@ import java.util.List;
 
 public class SearchCarResponse {
     private final List<Car> quertCars;
-
     public SearchCarResponse(List<Car> quertCars) {
         this.quertCars = quertCars;
     }
-
     public List<Car> getQuertCars() {
         return quertCars;
     }
