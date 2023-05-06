3a35bfeff4ca5946a05b79f8836f58e38cbe0a55
Bartłomiej Juda <bartekjkk@gmail.com>
2023-05-06 11:45:50 +0200

    refactor

--- src/main/java/pl/bartek/searchcar/controller/SearchCarController.java
+++ src/main/java/pl/bartek/searchcar/controller/SearchCarController.java
@@ -6,17 +6,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import pl.bartek.searchcar.Car;
 import pl.bartek.searchcar.service.SearchCarService;
-
 import java.util.List;
-
 @RestController
 public class SearchCarController {
 
+    private final SearchCarService searchCarService = new SearchCarService();
     @GetMapping("/find-car")
     SearchCarResponse searchCar(@RequestParam(name = "query",required = false,defaultValue = "") String query){
         SearchCarService searchCarService = new SearchCarService();
         List<Car> queryCars = searchCarService.findCar(query);
         return new SearchCarResponse(queryCars);
     }
-
 }
--- src/main/java/pl/bartek/searchcar/service/SearchCarService.java
+++ src/main/java/pl/bartek/searchcar/service/SearchCarService.java
@@ -1,15 +1,11 @@
 package pl.bartek.searchcar.service;
-
 import pl.bartek.searchcar.Car;
-
 import java.util.ArrayList;
 import java.util.List;
-
 public class SearchCarService {
     private final List<Car> carsRepository = new ArrayList<>();
     private final List<Car> queryCars = new ArrayList<>();
     public SearchCarService() {
-
         carsRepository.add(new Car("100","Wagon, Sedan", "1994"));
         carsRepository.add(new Car("100","Wagon, Sedan", "1993"));
         carsRepository.add(new Car("100","Wagon, Sedan", "1992"));
@@ -21,7 +17,6 @@ public class SearchCarService {
         carsRepository.add(new Car("A3","Sedan, Convertible", "2018"));
         carsRepository.add(new Car("A3","Sedan, Convertible", "2017"));
     }
-
     public List<Car> findCar(String query){
         for (Car car: carsRepository) {
             if (car.getCategory().contains(query)|| car.getModel().contains(query)|| car.getYear().contains(query)) {
