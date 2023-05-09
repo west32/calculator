20af07f63f16a7d32ca27bfd7a6eb1016132c630
Bartłomiej Juda <bartekjkk@gmail.com>
2023-05-09 07:41:07 +0200

    query list problem solved

--- src/main/java/pl/bartek/searchcar/service/SearchCarService.java
+++ src/main/java/pl/bartek/searchcar/service/SearchCarService.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 public class SearchCarService {
     private final List<Car> carsRepository = new ArrayList<>();
-    private final List<Car> queryCars = new ArrayList<>();
+
     public SearchCarService() {
         carsRepository.add(new Car("100","Wagon, Sedan", "1994"));
         carsRepository.add(new Car("100","Wagon, Sedan", "1993"));
@@ -18,6 +18,7 @@ public class SearchCarService {
         carsRepository.add(new Car("A3","Sedan, Convertible", "2017"));
     }
     public List<Car> findCar(String query){
+        List<Car> queryCars = new ArrayList<>();
         for (Car car: carsRepository) {
             if (car.getCategory().contains(query)|| car.getModel().contains(query)|| car.getYear().contains(query)) {
                 queryCars.add(car);
