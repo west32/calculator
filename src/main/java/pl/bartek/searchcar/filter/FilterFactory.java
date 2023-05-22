68fd6926688c849abe0d0ac19ea2ff82048e3f6e
Bartłomiej Juda <bartekjkk@gmail.com>
2023-05-22 20:47:25 +0200

    separated filter classes and refactored FilterFactory

--- src/main/java/pl/bartek/searchcar/filter/FilterFactory.java
+++ src/main/java/pl/bartek/searchcar/filter/FilterFactory.java
@@ -3,15 +3,20 @@ package pl.bartek.searchcar.filter;
 import pl.bartek.searchcar.filter.CarFilter;
 
 public class FilterFactory {
+    public CarFilter createModelFilter (String model){
+        return new ModelFilter(model);
+    }
 
-    public CarFilter createFilter(String parameter){
-        return switch (parameter) {
-            case "model" -> new ModelFilter(parameter);
-            case "category" -> new CategoryFilter(parameter);
-            case "fromYear" -> new FromYearFilter(parameter);
-            case "toYear" -> new ToYearFilter(parameter);
-            default -> null;
-        };
+    public CarFilter createCategoryFilter (String category){
+        return new CategoryFilter(category);
+    }
 
+    public CarFilter createFromYearFilter (Integer fromYear){
+        return new FromYearFilter(fromYear);
+    }
+
+    public CarFilter createToYearFilter (Integer toYear){
+        return new ToYearFilter(toYear);
     }
 }
+
